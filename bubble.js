406e5eb7e86fa33c39fb154040510da670e2f084
Kate Moser <komoser@gmail.com>
2022-04-15 14:07:13 -0500

    finished bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,24 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+
+  let currentPosition = 0;
+  let endpoint = nums.length;
+  while(endpoint > 0){
+    currentPosition = 0
+    while(currentPosition < endpoint){
+        if(nums[currentPosition] > nums[currentPosition +1]){
+            //swap
+            let temp = nums[currentPosition +1];
+            nums[currentPosition +1] = nums[currentPosition];
+            nums[currentPosition] = temp;
+        }
+        currentPosition++;
+    }
+    endpoint--;
+  }
+  return nums;
+}
 
 module.exports = bubbleSort;
+
+
+let nums = [3,7,2,9,1]
