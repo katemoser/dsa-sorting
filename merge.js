928ab09ad6e89179050d02dba002dfde6f6dcd04
edwinkim97 <edwinkim124@gmail.com>
2022-04-15 12:28:40 -0700

    finished on merge

--- merge.js
+++ merge.js
@@ -1,5 +1,33 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let out = [];
 
-function mergeSort() {}
+    let position1 = 0;
+    let position2 = 0;
+
+    while (position1 < arr1.length && position2 < arr2.length) {
+        if (arr1[position1] > arr2[position2]) {
+            out.push(arr2[position2]);
+            position2++;
+        }
+        else {
+            out.push(arr1[position1]);
+            position1++;
+        }
+    }
+
+    if (position1 >= arr1.length) {
+        out = out.concat(arr2.slice(position2));
+    }
+
+    if (position2 >= arr2.length) {
+        out = out.concat(arr1.slice(position1));
+    }
+
+    return out;
+}
+
+function mergeSort(arr) {
+    
+}
 
 module.exports = { merge, mergeSort};
